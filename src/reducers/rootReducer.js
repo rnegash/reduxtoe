7f5f3bbc6f9bbe8c3ec7d252f4ffaf02092256cc
rnegash <rnegash@macbookproc02w80gehv2g.berlin.internal.babbel.com>
2018-07-24 10:43:47 +0200

    player switch working

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -5,21 +5,22 @@ let initialState = { gameState: new Array(9), currentPlayer: 0 };
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case SPACE_CLICK:
-      console.log(action.spaceId);
-      break;
+      return state;
     case SWITCH_PLAYER:
-      return [
-        {
-          ...state,
-          currentPlayer: playerSwitch(state.currentPlayer)
-        }
-      ];
+      return {
+        ...state,
+        currentPlayer: playerSwitch(state.currentPlayer)
+      };
     default:
       return state;
   }
 }
 
 function playerSwitch(currentPlayer) {
+  console.log(currentPlayer);
+
   let nextPlayer = currentPlayer ? 0 : 1;
+  console.log(currentPlayer);
+
   return nextPlayer;
 }

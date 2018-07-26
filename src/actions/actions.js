2924c56b7e3f922be7c5ad3ef300b33ecdbcf916
rufael negash <rufael.negash@gmail.com>
2018-07-26 16:16:17 +0200

    add action+creator for playerwinnerid

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,6 @@
 export const SPACE_CLICK = "SPACE_CLICK";
 export const SWITCH_PLAYER = "SWITCH_PLAYER";
+export const SET_WINNER_ID = "SET_WINNER_ID";
 
 export function spaceClick(spaceId) {
   return { type: SPACE_CLICK, spaceId };
@@ -7,4 +8,8 @@ export function spaceClick(spaceId) {
 
 export function switchPlayer() {
   return { type: SWITCH_PLAYER };
+}
+
+export function setWinnerId(playerId) {
+  return { type: SET_WINNER_ID, playerId };
 }
